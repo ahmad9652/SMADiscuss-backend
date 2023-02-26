23a6645316225a79ae7ad4dda3bd02fde36ee520
Mohd. Ahmad <ahmadseemapuri@gmail.com>
2023-02-26 23:16:13 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Node server which will handle socket io
+// const io = require('socket.io')(8080)   
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+    cors: {
+              origin: 'http://127.0.0.1:5500',
+              methods: ['GET', 'POST'],
+              allowedHeaders: ["my-custom-header"],
+              credentials: true
+            }
+});
+
+
+httpServer.listen(8080);
+  
+const user = {};
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+        user[socket.id]=name;
+        console.log(name)
+        socket.broadcast.emit('user-joined',name);
+        console.log(name)
+    });
+    
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message: message , name: user[socket.id]});
+        
+    });
+
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',user[socket.id]);
+        delete user[socket.id];
+    });
+});
+
+
+  
